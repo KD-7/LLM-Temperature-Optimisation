c346b9ac889131c844d119a23366436d7b92200a
KD-7 <91730569+KD-7@users.noreply.github.com>
2025-03-06 23:22:55 +0000

    llama class

---
+++ TempEval/models/_llama.py
@@ -0,0 +1,31 @@
+import ollama
+
+
+class LLama:
+
+    def __init__(self, model_name: str, temperature: float, prompt: str):
+        self.model_name = model_name
+        self.temperature = temperature
+        self.prompt = prompt
+        ollama.pull(model_name)
+
+    def update_prompt(self, new_prompt: str):
+        """Set a new instruction prompt for the model
+        Args:
+            new_prompt (str): The new instruction prompt
+        """
+        self.prompt = new_prompt
+
+    def chat(self, data: str):
+        """Get model response to user input
+        Args:
+            data (str): The user input
+        Returns:
+            str: The response from the model
+        """
+        # No need to stream response for our use case
+        return ollama.chat(model=self.model_name,
+                           messages=[
+                               {'role': 'system', 'content': self.prompt},
+                               {'role': 'user', 'content': data}],
+                           options={'temperature': self.temperature})
--- llm_temp_optimisation/models/_llama.py
+++
@@ -1,9 +0,0 @@
-
-#TODO Setup config here make the only thing that we can modify the constructor with the temperature parameter
-
-class LLama3_1:
-    def __init__(self, temperature: float):
-        self.temperature = temperature
-
-    def _setup(self):
-        pass
