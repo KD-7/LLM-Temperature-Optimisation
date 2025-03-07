cdaad47c089ae250c6f89f3772e115a7d63627a5
KD-7 <91730569+KD-7@users.noreply.github.com>
2025-03-07 10:29:26 +0000

    refactor: dataset

--- TempEval/datasets/_ai4privacy.py
+++ TempEval/datasets/_ai4privacy.py
@@ -1,43 +1,40 @@
-import kagglehub
-import json
 
-def download_ai4privacy_dataset():
-    """Downloads the AI4Privacy dataset from Kaggle and returns the path to the dataset"""
-    # Download latest version
-    path = kagglehub.dataset_download("verracodeguacas/ai4privacy-pii")
-    return path
+from datasets import load_dataset
 
 
-def load_ai4privacy_dataset(path):
-    """Loads the AI4Privacy dataset from the given path and returns the four data
-    types as lists
+def download_ai4privacy_dataset(file_specifier: None, dataset_specifier: None):
+    """Downloads and creates an instance of the dataset
 
     Args:
-        path (str): The path to the AI4Privacy dataset
+        file_specifier: Select specific files within the dataset
+        dataset_specifier: Select a specific variant of the dataset e.g. test(validation)
+         or train
+        """
+    # Download latest version
+    return load_dataset("ai4privacy/pii-masking-200k", data_files=file_specifier,
+                        split=dataset_specifier)
+
+
+def load_ai4privacy_dataset(dataset):
+    """Loads the AI4Privacy dataset returns a tuple of lists
+
+    Args:
+        dataset: Instance of AI4Privacy dataset
 
     Returns: A tuple of (masked_text, unmasked_text, token_entity_labels,
     tokenised_unmasked_text)
 
-        - "masked_text" (list[str]): A list of texts with personally identifiable
-        information (PII) replaced by placeholder tokens.
+        - "source_text" (list[str]): A list of original texts containing PII.
 
-        - "unmasked_text" (list[str]): A list of original texts containing PII.
+        - "target_text" (list[str]): A list of texts with personally identifiable
+        information (PII) replaced by placeholder tokens, serving as the ground truth.
 
-        - "token_entity_labels" (list[str]): A list containing entity labels
+        - "privacy_mask" (list[str]): A list containing entity labels
         corresponding to tokens in the text, identifying
         the type of PII ( e.g., "NAME", "EMAIL").
-
-        - "tokenised_unmasked_text" (list[str]): A list of tokenised unmasked text.
     """
+    source_text = [entry["source_text"] for entry in dataset]
+    target_text = [entry["target_text"] for entry in dataset]
+    privacy_mask = [entry["privacy_mask"] for entry in dataset]
 
-    # absolute path
-    dataset_english_variant = path + "\pii-masking-65k\english_balanced_10k.jsonl"
-    with open(dataset_english_variant, "r", encoding="utf-8") as f:
-        data = [json.loads(line) for line in f]  # each line is a json object
-
-    masked_text = [entry["masked_text"] for entry in data]
-    unmasked_text = [entry["unmasked_text"] for entry in data]
-    token_entity_labels = [entry["token_entity_labels"] for entry in data]
-    tokenised_unmasked_text = [entry["tokenised_unmasked_text"] for entry in data]
-
-    return masked_text, unmasked_text, token_entity_labels, tokenised_unmasked_text
+    return source_text, target_text, privacy_mask
