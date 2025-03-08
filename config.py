e2abee8306251a6711acfab8ca389e2825655b02
KD-7 <91730569+KD-7@users.noreply.github.com>
2025-03-08 20:49:07 +0000

    refactor: move additional config into config file

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+
+# For exporting results to GitHub
+AUTH_TOKEN = "YOUR_GITHUB_AUTH_TOKEN"
+REPO_PATH = "KD-7/LLM-Temperature-Optimisation"
+BRANCH_NAME = "github_pages"
+
+# Experiment Configuration
+TEMPERATURE_VALUES = [0.2, 0.4, 0.6, 0.8, 1.0]
+DATA_POINTS = 5  # Change to 40000 for full dataset
+SAMPLE_POINTS = 10  # The no. of entries to collect for survey sample
+ITERATIONS = 5
+SAVE_DIR = "results"
+MODEL_NAME = "llama3" #TODO: what about 3.1?? check the params
+PROMPT = "put the prompt here"
+RANDOM_SEED = 42
+PYTHON_VERSION = "3.10"
+
+# Ensure save directory exists
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+config_settings = {
+    "TEMPERATURE_VALUES": TEMPERATURE_VALUES,
+    "DATA_POINTS": DATA_POINTS,
+    "SAMPLE_POINTS": SAMPLE_POINTS,
+    "ITERATIONS": ITERATIONS,
+    "SAVE_DIR": SAVE_DIR,
+    "MODEL_NAME": MODEL_NAME,
+    "PROMPT": PROMPT,
+    "RANDOM_SEED": RANDOM_SEED,
+    "PYTHON_VERSION": PYTHON_VERSION
+}
--- experiment.py
+++ experiment.py
@@ -1,81 +1,92 @@
 # This script runs an experiment using the LLama model on the AI4Privacy dataset.
 
-import os
 import random
-import pandas as pd
+from config import *
 from tempeval import datasets, models, utils
 from tempeval.utils import FileHandler
 
-# For exporting results to GitHub
-AUTH_TOKEN = "YOUR_GITHUB_AUTH_TOKEN"
-REPO_PATH = "YOUR_GITHUB_REPO_PATH"
-BRANCH_NAME = "main"
-
-# Experiment configuration
-TEMPERATURE_VALUES = [0.2, 0.4, 0.6, 0.8, 1.0]
-DATA_POINTS = 5  # Change to 40000 for full dataset
-ITERATIONS = 5
-SAVE_DIR = "results"
-MODEL_NAME = "llama3"  # what about 3.1?? check the params
-PROMPT = "put the prompt here"
-RANDOM_SEED = 42
+# Set random seed for reproducibility
 random.seed(RANDOM_SEED)
-
-# Ensure save directory exists
-os.makedirs(SAVE_DIR, exist_ok=True)
 
 # Dataset configuration
 dataset = datasets.download_ai4privacy_dataset("english_pii_43k.jsonl", "train")
 source_text, target_text, privacy_mask = datasets.load_ai4privacy_dataset(dataset)
 
 DATA_SET_SIZE = len(dataset)
-RANDOM_SAMPLE_INDICES = random.sample(range(DATA_SET_SIZE), DATA_POINTS)
-GROUND_TRUTH = [target_text[i] for i in RANDOM_SAMPLE_INDICES]
+
+# Used to collect a random sample of results for survey
+RANDOM_SAMPLE_INDICES = random.sample(range(DATA_SET_SIZE), SAMPLE_POINTS)
 
 # Model configuration
 model = models.LLama(MODEL_NAME, PROMPT)
 
-for temperature in TEMPERATURE_VALUES:
+file_handler = FileHandler(SAVE_DIR)
 
-    # Create a new file handler for each temperature setting [CHECK THSI!!!!!HIS
-    file_handler = FileHandler(SAVE_DIR)
-    excel_filepath = file_handler.get_excel_filename(temperature, 0)
-    file_handler.initialize_excel_file(excel_filepath)
+for temperature in TEMPERATURE_VALUES:
 
     iteration_results = []
     for iteration in range(ITERATIONS):
 
-        model_response = []
-        excel_filepath = FileHandler.get_excel_filename(temperature, iteration)
-        FileHandler.initialize_excel_file(excel_filepath)
+        excel_filepath = file_handler.get_excel_filename(temperature, iteration)
+        file_handler.initialize_excel_file(excel_filepath)
 
-        for data_point in RANDOM_SAMPLE_INDICES:
-            model_response.append(model.chat(source_text[data_point], temperature))
+        rogue_1, rogue_2, rogue_l = 0, 0, 0
+        precision, recall, f1 = 0, 0, 0
 
-        # TODO: the metrics does it one by one , so i need to chang ehere to only input one resposne
-        rogue_1, rogue_2, rogue_l = utils.text_similarity_metrics(model_response,
-                                                                  GROUND_TRUTH)
-        precision, recall, f1 = utils.anonymisation_metrics(model_response,
-                                                            target_text[data_point])
+        sample_responses = []
 
-        run_results = pd.DataFrame({
-            'Raw text': source_text[RANDOM_SAMPLE_INDICES],
-            'Ground truth': GROUND_TRUTH,
-            'Anonymised text': model_response,
+        for data_point in range(DATA_POINTS):
+
+            model_response = utils.sanitise_response(model.chat(source_text[data_point],
+                                                                temperature))
+
+            r1, r2, rl = utils.text_similarity_metrics(model_response[data_point],
+                                                       target_text[data_point])
+            rogue_1 += r1; rogue_2 += r2; rogue_l += rl
+
+            p, r, f = utils.anonymisation_metrics(model_response[data_point],
+                                                  target_text[data_point],
+                                                  privacy_mask[data_point])
+            precision += p; recall += r; f1 += f
+
+            if data_point in RANDOM_SAMPLE_INDICES:
+                sample_responses.append({
+                    'Source text': source_text[data_point],
+                    'Ground truth': target_text[data_point],
+                    'Model response': model_response,
+                    'Precision': p,
+                    'Recall': r,
+                    'F1': f,
+                    'ROUGE-1': r1,
+                    'ROUGE-2': r2,
+                    'ROUGE-L': rl
+                })
+
+        # Average over all data points
+        rogue_1 /= DATA_POINTS; rogue_2 /= DATA_POINTS; rogue_l /= DATA_POINTS
+        precision /= DATA_POINTS; recall /= DATA_POINTS; f1 /= DATA_POINTS
+
+        run_results = {
+            'Run': iteration,
+            'Temperature': temperature,
             'Precision': precision,
             'Recall': recall,
             'F1': f1,
             'ROUGE-1': rogue_1,
             'ROUGE-2': rogue_2,
             'ROUGE-L': rogue_l
-        })
-        # TODO: why no accuracy score?
-        FileHandler.save_results_to_excel(excel_filepath, run_results)
+        }
+        # TODO differentiate saving sample results and overall results
+        # TODO export configs to excel
+        # TODO: SAVING SUMMARY OF RUN TO EXCEL
+        file_handler.save_results_to_excel(excel_filepath, sample_responses)
 
         iteration_results.append(run_results)
 
-    utils.export_results_to_github(iteration_results, temperature, REPO_PATH,
-                                   BRANCH_NAME,
-                                   AUTH_TOKEN)
+    utils.export_results_github(iteration_results, temperature, REPO_PATH,
+                                BRANCH_NAME,
+                                AUTH_TOKEN)
+    # TODO: define config
+    utils.export_config_github(config_settings, REPO_PATH, BRANCH_NAME, AUTH_TOKEN)
 
 print("Experiment completed successfully.")
