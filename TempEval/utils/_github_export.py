34d362d9f72d3a022d93ecfd4bd28c812702b80e
KD-7 <91730569+KD-7@users.noreply.github.com>
2025-03-08 20:29:48 +0000

    add: support for config export

--- TempEval/utils/_github_export.py
+++ TempEval/utils/_github_export.py
@@ -2,28 +2,52 @@ import pandas as pd
 from github import Auth, Github
 
 
-def export_to_github(data, temp_value, repo_path, branch_name, auth_token=None):
+def _get_repo(auth_token, repo_path):
+    auth = Auth.Token(auth_token)
+    return Github(auth=auth).get_repo(repo_path)
+
+
+def export_config_github(config, repo_path, branch_name, auth_token=None):
+    """Exports the given config to the CONFIG.md markdown file in the specified GitHub
+    repository
+
+    Args:
+        config (dict): A dictionary containing the configuration settings for the
+        experiment.
+
+        repo_path (str): The path to the GitHub repository in the format "owner/repo".
+
+        branch_name (str): The name of the branch to push the changes to.
+
+        auth_token (str): The GitHub personal access token to authenticate the user.
+    """
+    df = pd.DataFrame(config)
+    repo = _get_repo(auth_token, repo_path)
+    old_contents = repo.get_contents("CONFIG.md", ref=branch_name)
+    new_contents = (old_contents.decoded_content.decode() + "\n" +
+                    "## Experiment Configuration\n" +
+                    df.to_markdown(index=False))
+    repo.update_file(old_contents.path, "Updated config", new_contents,
+                     branch=branch_name, sha=old_contents.sha)
+
+    print("Config exported to github successfully")
+
+
+def export_results_github(data, temp_value, repo_path, branch_name, auth_token=None):
     """Exports the given data to the RESULTS.md markdown file in the specified GitHub
     repository
 
-    Args: data (list[list]): A list of lists containing the results for each column
-    in table. Format is as follows: [Run, Accuracy, Precision, Recall, F1, ROUGE-1,
-    ROUGE-2, ROUGE-L, Temperature, Python Version]"""
+    Args: data_frame (dict): A dictionary containing the results for the experiment.
+    """
 
-    # Create a dataframe
-    headers = ["Run", "Accuracy", "Precision", "Recall", "F1",
-               "ROUGE-1", "ROUGE-2", "ROUGE-L", "Temperature", "Python Version"]
-
-    df = pd.DataFrame(data, columns=headers)
-
-    # Export to GitHub
-    auth = Auth.Token(auth_token)
-    g = Github(auth=auth)
-    repo = g.get_repo(repo_path)
+    repo = _get_repo(auth_token, repo_path)
+    df = pd.DataFrame(data)
     old_contents = repo.get_contents("RESULTS.md", ref=branch_name)
+
     new_contents = (old_contents.decoded_content.decode() + "\n" +
                     "## Temperature value = " + str(temp_value) + "\n" +
                     df.to_markdown(index=False))
     repo.update_file(old_contents.path, "Updated results", new_contents,
                      branch=branch_name, sha=old_contents.sha)
+
     print("Results exported to github successfully")
