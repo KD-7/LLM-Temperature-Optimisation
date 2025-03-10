d9659b02bb7ba2b2942d4668d621d6eb5f9a630a
KD-7 <91730569+KD-7@users.noreply.github.com>
2025-03-10 13:41:32 +0000

    remove print statements

--- temp_eval/tests/test_sanitisation.py
+++ temp_eval/tests/test_sanitisation.py
@@ -1,13 +1,13 @@
 from temp_eval.utils import sanitise_response
 
+#TODO: fix the ERRORS!
 class TestSanitisation:
     
     def test_left_padding(self):
         source_text = "Alice and Bob are friends"
         response = "I can anonymise this: [NAME] and [NAME] are friends"
         assert sanitise_response(source_text, response) == "['[NAME]', 'and', '[NAME]', 'are', 'friends']"
-        print("test_left_padding: OK")
-        
+
     def test_right_padding(self):
         source_text = "Alice and Bob are friends"
         response = "[NAME] and [NAME] are friends, this is the correct anonymisation"
@@ -24,8 +24,7 @@ class TestSanitisation:
         source_text = "Alice and Bob are friends"
         response = "This is the correct anonymisation:\n [NAME] and [NAME] are friends"
         assert sanitise_response(source_text, response) == "['[NAME]', 'and', '[NAME]', 'are', 'friends']"
-        print("test_left_newline_padding: OK")
-        
+
     def test_right_newline_padding(self):
         source_text = "Alice and Bob are friends"
         response = "[NAME] and [NAME] are friends\n This is the correct anonymisation"
@@ -44,8 +43,7 @@ class TestSanitisation:
         
         [NAME] and [NAME] are friends"""
         assert sanitise_response(source_text, response) == "['[NAME]', 'and', '[NAME]', 'are', 'friends']"
-        print("test_left_multiline_padding: OK")
-        
+
     def test_right_multiline_padding(self):
         source_text = "Alice and Bob are friends"
         response = """[NAME] and [NAME] are friends
